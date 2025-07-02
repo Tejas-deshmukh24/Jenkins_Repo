6c52988af3e2c68debb8c36d04e8348f1701167b
Tejas Deshmukh <tejasdeshmukh11223@gmail.com>
2025-07-02 12:57:44 +0530

    PUSH

--- src/test/java/Package2/OrgTest.java
+++ src/test/java/Package2/OrgTest.java
@@ -7,6 +7,16 @@ public class OrgTest {
 	@Test
 	public void createcOrgTest() {
 		System.out.println("Execute createOrgTest successful");
+		String url=System.getProperty("url","http://localhost:8080/");
+		String browser=System.getProperty("browser","chrome");
+		String username=System.getProperty("username","admin");
+		String password=System.getProperty("password","admin");
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("Execute createOrgTest");
 	}
 	
 	@Test
