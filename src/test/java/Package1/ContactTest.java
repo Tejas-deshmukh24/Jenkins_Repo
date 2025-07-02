fbefad6dd257ce02891db3166b35fe3d2f79d7cf
Tejas Deshmukh <tejasdeshmukh11223@gmail.com>
2025-07-02 10:05:52 +0530

    first push

---
+++ src/test/java/Package1/ContactTest.java
@@ -0,0 +1,29 @@
+package Package1;
+
+import org.apache.commons.collections4.bag.SynchronizedSortedBag;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createcontactTest() {
+		
+		String url=System.getProperty("url","http://localhost:8080/");
+		String browser=System.getProperty("browser","chrome");
+		String username=System.getProperty("username","admin");
+		String password=System.getProperty("password","admin");
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("Execute createcontactTest");
+	}
+	
+	@Test
+	public void modifycontactTest() {
+		System.out.println("Execute modifycontactTest");
+	}
+	
+
+}
---
+++ src/test/java/Package2/OrgTest.java
@@ -0,0 +1,18 @@
+package Package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createcOrgTest() {
+		System.out.println("Execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Execute modifyOrgTest");
+	}
+	
+
+}
